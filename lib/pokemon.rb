4626030f3f887f82e715354e4e2080918fc41b8a
Samantha Jordan <shinyguns@gmail.com>
2020-04-06 19:18:28 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  
+  attr_reader :id, :name, :type, :hp, :db
+  @@all = []
+  
+  def initialize (id:, name:, type:, hp: nil, db:)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+    @@all << self
+  end
+  
 end
